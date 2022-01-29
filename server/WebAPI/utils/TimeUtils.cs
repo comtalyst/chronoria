fae285f82cedea154f9e4b5b08d0cbee894d8e97
comtalyst <comtalyst@outlook.com>
2022-01-28 18:55:00 -0600

    Add TimeUtils

---
+++ server/WebAPI/utils/TimeUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace Chronoria_WebAPI.utils
+{
+    public class TimeUtils
+    {
+        public static long DateTimeToEpochMs(DateTime dt)
+        {
+            return new DateTimeOffset(dt).ToUnixTimeMilliseconds();
+        }
+        public static DateTime EpochMsToDateTime(long ms)
+        {
+            return DateTimeOffset.FromUnixTimeMilliseconds(ms).DateTime;
+        }
+    }
+}
