bc64b3d26dc014c35e845d731121d399d052d12f
comtalyst <comtalyst@outlook.com>
2022-02-08 17:28:45 -0600

    Fix BlocklistEntry

--- server/WebAPI/Models/BlocklistEntry.cs
+++ server/WebAPI/Models/BlocklistEntry.cs
@@ -3,8 +3,16 @@
     public class BlocklistEntry
     {
         public string Email { get; set; }
-        public string Capture { get; set; }
+        public DateTime Capture { get; set; }
         public DateTime Release { get; set; }
         public string Reasons { get; set; }
+
+        public BlocklistEntry(string Email, DateTime Capture, DateTime Release, string Reasons)
+        {
+            this.Email = Email;
+            this.Capture = Capture;
+            this.Release = Release;
+            this.Reasons = Reasons;
+        }
     }
 }
