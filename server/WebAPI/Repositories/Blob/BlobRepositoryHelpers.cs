a0125f3ca2a7cdfd7b2674b3eb4df690b2ecef33
comtalyst <comtalyst@outlook.com>
2022-02-08 00:22:25 -0600

    Share blob repo codes by adding an abstract level

---
+++ server/WebAPI/Repositories/Blob/BlobRepositoryHelpers.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+
+namespace Chronoria_WebAPI.Repositories
+{
+    public abstract class BlobRepositoryHelpers<BlobServiceClientType> where BlobServiceClientType : BlobServiceClient
+    {
+        protected readonly BlobServiceClientType _client;
+        protected readonly string _containerName;
+        protected readonly BlobContainerClient blobContainerClient;
+
+        public BlobRepositoryHelpers(BlobServiceClientType client, string containerName)
+        {
+            _client = client;
+            _containerName = containerName;
+            blobContainerClient = _client.CreateBlobContainer(containerName);
+        }
+
+        protected BlobClient GetClient(string blobFileName)
+        {
+            return blobContainerClient.GetBlobClient(blobFileName);
+        }
+
+        public Uri GetTransferUri(string blobFileName)
+        {
+            DateTimeOffset expiredOn = DateTimeOffset.UtcNow.AddMinutes(60);            // TODO
+            BlobClient blobClient = GetClient(blobFileName);
+            Uri uri = blobClient.GenerateSasUri(BlobSasPermissions.Read, expiredOn);
+            return uri;
+        }
+
+        public async Task ReceiveTransfer(string blobFileName, Uri uri)
+        {
+            BlobClient blobClient = GetClient(blobFileName);
+            CopyFromUriOperation ops = await blobClient.StartCopyFromUriAsync(uri);
+
+            // check progress
+            BlobProperties properties = (await blobClient.GetPropertiesAsync()).Value;
+            while (!ops.HasCompleted)
+            {
+                long copied = await ops.WaitForCompletionAsync();
+
+                Console.WriteLine($"Blob: {blobFileName}, Copied: {copied} of {properties.ContentLength}");
+                await Task.Delay(500);          // TODO
+            }
+        }
+    }
+}
--- server/WebAPI/Repositories/Blob/FileBlobRepository.cs
+++ server/WebAPI/Repositories/Blob/FileBlobRepository.cs
@@ -5,23 +5,13 @@ using Chronoria_WebAPI.Models;
 
 namespace Chronoria_WebAPI.Repositories
 {
-    public class FileBlobRepository<BlobServiceClientType> : IFileBlobRepository<BlobServiceClientType> where BlobServiceClientType : BlobServiceClient
+    public class FileBlobRepository<BlobServiceClientType> : BlobRepositoryHelpers<BlobServiceClientType>, IFileBlobRepository<BlobServiceClientType> where BlobServiceClientType : BlobServiceClient
     {
         protected readonly BlobServiceClientType _client;
         protected readonly string _containerName;
         protected readonly BlobContainerClient blobContainerClient;
 
-        public FileBlobRepository(BlobServiceClientType client, string containerName)
-        {
-            _client = client;
-            _containerName = containerName;
-            blobContainerClient = _client.CreateBlobContainer(containerName);
-        }
-
-        private BlobClient GetClient(string blobFileName)
-        {
-            return blobContainerClient.GetBlobClient(blobFileName);
-        }
+        public FileBlobRepository(BlobServiceClientType client, string containerName) : base(client, containerName) { }
 
         public async Task<BlobFile> Create(BlobFile entry)
         {
@@ -41,30 +31,6 @@ namespace Chronoria_WebAPI.Repositories
         public async Task<BlobFile> Get(string blobFileName)
         {
             throw new NotImplementedException();
-        }
-
-        public Uri GetTransferUri(string blobFileName)
-        {
-            DateTimeOffset expiredOn = DateTimeOffset.UtcNow.AddMinutes(60);            // TODO
-            BlobClient blobClient = GetClient(blobFileName);
-            Uri uri = blobClient.GenerateSasUri(BlobSasPermissions.Read, expiredOn);
-            return uri;
-        }
-
-        public async Task ReceiveTransfer(string blobFileName, Uri uri)
-        {
-            BlobClient blobClient = GetClient(blobFileName);
-            CopyFromUriOperation ops = await blobClient.StartCopyFromUriAsync(uri);
-
-            // check progress
-            BlobProperties properties = (await blobClient.GetPropertiesAsync()).Value;
-            while (!ops.HasCompleted)
-            {
-                long copied = await ops.WaitForCompletionAsync();
-
-                Console.WriteLine($"Blob: {blobFileName}, Copied: {copied} of {properties.ContentLength}");
-                await Task.Delay(500);          // TODO
-            }
         }
     }
 }
