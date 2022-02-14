712a5d77a8c5077c750791e55e0a587779ea96ec
comtalyst <comtalyst@outlook.com>
2022-02-14 02:31:41 -0600

    Define keys

--- server/WebAPI/Models/DbContexts/BaseContext.cs
+++ server/WebAPI/Models/DbContexts/BaseContext.cs
@@ -7,6 +7,12 @@ namespace Chronoria_WebAPI.Models
         public BaseContext(DbContextOptions options) : base(options) {
             Database.EnsureCreated();
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Capsule>().HasKey(entry => entry.Id);
+            modelBuilder.Entity<FileContent>().HasKey(entry => entry.Id);
+            modelBuilder.Entity<TextContent>().HasKey(entry => entry.Id);
+        }
 
         // Each DbSet will map to a table in the database
         public DbSet<Capsule> Capsules { get; set; }
--- server/WebAPI/Models/DbContexts/MetaContext.cs
+++ server/WebAPI/Models/DbContexts/MetaContext.cs
@@ -5,6 +5,10 @@ namespace Chronoria_WebAPI.Models
     public class MetaContext : DbContext
     {
         public MetaContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BlocklistEntry>().HasKey(entry => entry.Email);
+        }
 
         // Each DbSet will map to a table in the database
         public DbSet<BlocklistEntry> BlocklistEntries { get; set; }
