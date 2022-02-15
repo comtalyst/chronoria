d83a69dd049707d348e91740b70ac5ac16502484
comtalyst <comtalyst@outlook.com>
2022-02-15 01:34:44 -0600

    Add PingController

---
+++ server/WebAPI/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chronoria_WebAPI.Controllers
+{
+    [Route("api/ping/")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
