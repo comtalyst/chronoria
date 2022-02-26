458287bebfffe0658968ec91a546164504039d50
comtalyst <comtalyst@outlook.com>
2022-02-26 02:37:18 -0600

    Draft DownloadLinkController

---
+++ server/WebAPI/Controllers/DownloadLinkController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Chronoria_WebAPI.Services;
+using Chronoria_WebAPI.Models;
+
+namespace Chronoria_WebAPI.Controllers
+{
+    [Route("api/download")]
+    [ApiController]
+    public class DownloadLinkController : ControllerBase
+    {
+        //IDownloadLinkService downloadLinkService;
+        IRequestValidationService requestValidationService;
+        IIdMatchingService<ActiveContext> idMatchingService;
+
+        public DownloadLinkController(
+            //IDownloadLinkService downloadLinkService,
+            IRequestValidationService requestValidationService,
+            IIdMatchingService<ActiveContext> idMatchingService
+            )
+        {
+            //this.downloadLinkService = downloadLinkService;
+            this.requestValidationService = requestValidationService;
+            this.idMatchingService = idMatchingService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Download(string id, string receipientEmail)
+        {
+            try
+            {
+                id = id.Trim();
+                receipientEmail = receipientEmail.Trim();
+                requestValidationService.ValidateId(id);
+                requestValidationService.ValidateEmail(receipientEmail);
+
+                if (!await idMatchingService.MatchReceipientEmail(id, receipientEmail))
+                {
+                    throw new RejectException(RejectException.VerificationFailed);
+                }
+                throw new NotImplementedException();
+                //return Ok(await downloadLinkService.GetLink(id));
+            }
+            catch (RejectException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // TODO: log ex
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
