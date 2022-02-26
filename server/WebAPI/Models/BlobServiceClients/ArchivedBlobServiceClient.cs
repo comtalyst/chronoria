29408edaa90a92b483112044a7c9ec9d1b3a5ed2
comtalyst <comtalyst@outlook.com>
2022-02-26 01:47:36 -0600

    Add ArchivedBlobServiceClient

---
+++ server/WebAPI/Models/BlobServiceClients/ArchivedBlobServiceClient.cs
@@ -0,0 +1,9 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Chronoria_WebAPI.Models
+{
+    public class ArchivedBlobServiceClient : BlobServiceClient
+    {
+        public ArchivedBlobServiceClient(string connectionString) : base(connectionString) { }
+    }
+}
