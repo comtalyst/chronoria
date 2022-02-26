a6910aa70162a75a3072608fc53c9228310cffa5
comtalyst <comtalyst@outlook.com>
2022-02-26 01:59:47 -0600

    Add CanceledReceiptEmailProducer

---
+++ server/WebAPI/Models/CanceledReceiptEmailMessage.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace Chronoria_WebAPI.Models
+{
+    public class CanceledReceiptEmailMessage : IMessage
+    {
+        public string Email { get; set; }
+        public string Ref { get; set; }
+        public string RecipientName { get; set; }
+        public string RecipientEmail { get; set; }
+        public long SendTime { get; set; }
+
+        public CanceledReceiptEmailMessage(string Email, string Ref, string RecipientName, string RecipientEmail, long SendTime)
+        {
+            this.Email = Email;
+            this.Ref = Ref;
+            this.RecipientName = RecipientName;
+            this.RecipientEmail = RecipientEmail;
+            this.SendTime = SendTime;
+        }
+        public string Jsonify()
+        {
+            var obj = new
+            {
+                _meta = new                                         // TODO: share this meta code on some parental level
+                {
+                    MessageClass = "CanceledReceiptEmailMessage",
+                    Sender = "Chronoria-WebAPI"
+                },
+                Email = Email,
+                Ref = Ref,
+                RecipientName = RecipientName,
+                RecipientEmail = RecipientEmail,
+                SendTime = SendTime
+            };
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
---
+++ server/WebAPI/Producers/CanceledReceiptEmailProducer.cs
@@ -0,0 +1,9 @@
+﻿using Chronoria_WebAPI.Models;
+
+namespace Chronoria_WebAPI.Producers
+{
+    public class CanceledReceiptEmailProducer : GeneralProducer<CanceledReceiptEmailMessage>, ICanceledReceiptEmailProducer
+    {
+        public CanceledReceiptEmailProducer(string connectionString) : base(connectionString, "CanceledReceiptEmailTopic") { }
+    }
+}
