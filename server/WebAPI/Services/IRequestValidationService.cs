d699162c93a1820fc85b78cce972f52996bdcf5d
comtalyst <comtalyst@outlook.com>
2022-02-25 23:48:21 -0600

    Remove old messages

--- server/WebAPI/Services/IRequestValidationService.cs
+++ server/WebAPI/Services/IRequestValidationService.cs
@@ -4,14 +4,6 @@ namespace Chronoria_WebAPI.Services
 {
     public interface IRequestValidationService
     {
-        protected const string InvalidEmail = "INVALID_EMAIL";
-        protected const string InvalidName = "INVALID_NAME";
-        protected const string InvalidFutureTime = "INVALID_FUTURE_TIME";
-        protected const string InvalidText = "INVALID_TEXT";
-        protected const string InvalidTextLoc = "INVALID_TEXT_LOC";
-        protected const string InvalidFile = "INVALID_FILE";
-        protected const string InvalidId = "INVALID_ID";
-
         public void ValidateEmail(string email);
         public void ValidateName(string name);
         public void ValidateFutureTime(long time);
