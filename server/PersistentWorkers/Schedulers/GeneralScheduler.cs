00f332fdd1f3ae57100eca77b101a6959a1255d4
comtalyst <comtalyst@outlook.com>
2022-03-01 02:45:30 -0600

    WIP schedulers

---
+++ server/PersistentWorkers/Schedulers/GeneralScheduler.cs
@@ -0,0 +1,57 @@
+﻿namespace Chronoria_PersistentWorkers.Schedulers
+{
+    public abstract class GeneralScheduler : IScheduler
+    {
+        private long lastTime;
+        private bool run=false;
+        public async Task Start()
+        {
+            lastTime = await LastTime();
+            run = true;
+            _ = Looper();
+        }
+
+        public async Task Suspend()
+        {
+            run = false;
+        }
+
+        private async Task Looper()
+        {
+            while (run)
+            {
+                // Trigger using the time interval for parameters
+                long curTime = 0;    // TODO
+                await Trigger(lastTime + 1, curTime);
+
+                // Update lastTime to current time
+                lastTime = curTime;
+                await SetLastTime(lastTime);
+
+                // Calculate sleep time to be min(fetchTime, nextTime - curTime (newly defined for accuracy))
+                long nextTime = await NextTime(curTime);
+                long fetchTime = await FetchTime();
+                long sleepTime;
+                curTime = 0;    // TODO
+                if (nextTime - curTime > fetchTime)
+                {
+                    sleepTime = fetchTime;
+                }
+                else
+                {
+                    sleepTime = nextTime - curTime;
+                }
+                if (sleepTime > 0)
+                {
+                    await Task.Delay((int)sleepTime);
+                }
+            }
+        }
+
+        protected abstract Task Trigger(long lastTime, long curTime);
+        protected abstract Task<long> NextTime(long curTime);
+        protected abstract Task<long> LastTime();
+        protected abstract Task<long> FetchTime();
+        protected abstract Task SetLastTime(long lastTime);
+    }
+}
---
+++ server/PersistentWorkers/Schedulers/IScheduler.cs
@@ -0,0 +1,8 @@
+﻿namespace Chronoria_PersistentWorkers.Schedulers
+{
+    public interface IScheduler
+    {
+        public Task Start();
+        public Task Suspend();
+    }
+}
