1de67cde05a8dba555b9438467a11aed7c6c1872
comtalyst <comtalyst@outlook.com>
2022-03-02 17:06:02 -0600

    Update Service Bus

---
+++ server/PersistentWorkers/Schedulers/ExpireClearScheduler.cs
@@ -0,0 +1,30 @@
+﻿namespace Chronoria_PersistentWorkers.Schedulers
+{
+    public class ExpireClearScheduler : GeneralScheduler, IScheduler
+    {
+        protected override Task<long> FetchTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<long> LastTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<long> NextTime(long curTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task SetLastTime(long lastTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task Trigger(long lastTime, long curTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
