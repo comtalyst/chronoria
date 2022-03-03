605002f4a2d11137e855f73fc4dc5fa6a4cc4152
comtalyst <comtalyst@outlook.com>
2022-03-02 18:33:30 -0600

    Implements ExpireClearScheduler

--- server/PersistentWorkers/Schedulers/ExpireClearScheduler.cs
+++ server/PersistentWorkers/Schedulers/ExpireClearScheduler.cs
@@ -1,30 +1,58 @@
-﻿namespace Chronoria_PersistentWorkers.Schedulers
+﻿using Chronoria_PersistentWorkers.Producers;
+using Chronoria_PersistentWorkers.Repositories;
+using Chronoria_PersistentWorkers.Models;
+using Chronoria_PersistentWorkers.utils;
+
+namespace Chronoria_PersistentWorkers.Schedulers
 {
     public class ExpireClearScheduler : GeneralScheduler, IScheduler
     {
-        protected override Task<long> FetchTime()
+        private readonly long fetchTime;
+        private readonly IExpireClearProducer expireClearProducer;
+        private readonly ICapsuleRepository<PendingContext> pendingCapsuleRepository;
+
+        public ExpireClearScheduler(
+            long fetchTime,
+            IExpireClearProducer expireClearProducer,
+            ICapsuleRepository<PendingContext> pendingCapsuleRepository
+            )
         {
-            throw new NotImplementedException();
+            this.fetchTime = fetchTime;
+            this.expireClearProducer = expireClearProducer;
+            this.pendingCapsuleRepository = pendingCapsuleRepository;
         }
 
-        protected override Task<long> LastTime()
+        protected override long FetchTime()
         {
-            throw new NotImplementedException();
+            return fetchTime;
         }
 
-        protected override Task<long> NextTime(long curTime)
+        protected override async Task<long> LastTime()
         {
-            throw new NotImplementedException();
+            // TODO
+            return 0;
         }
 
-        protected override Task SetLastTime(long lastTime)
+        protected override async Task<long> NextTime(long curTime)
         {
-            throw new NotImplementedException();
+            var nextCapsule = await pendingCapsuleRepository.GetNextByCreateTime(TimeUtils.EpochMsToDateTime(curTime));
+            if(nextCapsule == null)
+            {
+                return long.MaxValue;
+            }
+            return TimeUtils.DateTimeToEpochMs(nextCapsule.CreateTime);
         }
 
-        protected override Task Trigger(long lastTime, long curTime)
+        protected override async Task SetLastTime(long lastTime)
         {
-            throw new NotImplementedException();
+            // TODO
+            return;
+        }
+
+        protected override async Task Trigger(long lastTime, long curTime)
+        {
+            ExpireClearMessage expireClearMessage = new ExpireClearMessage(TimeUtils.EpochMsToDateTime(lastTime), TimeUtils.EpochMsToDateTime(curTime));
+            await expireClearProducer.Produce(expireClearMessage);
         }
     }
 }
