4db87e7e517ecdab0bf682a0420a785406ce484b
comtalyst <comtalyst@outlook.com>
2022-03-02 23:00:47 -0600

    Add missing models

---
+++ server/PersistentWorkers/Models/TextContent.cs
@@ -0,0 +1,17 @@
+﻿namespace Chronoria_PersistentWorkers.Models
+{
+    public class TextContent
+    {
+        public string Id { get; set; }
+        public string TextFileId { get; set; }
+
+        public TextContent(
+            string Id,
+            string TextFileId
+        )
+        {
+            this.Id = Id;
+            this.TextFileId = TextFileId;
+        }
+    }
+}
