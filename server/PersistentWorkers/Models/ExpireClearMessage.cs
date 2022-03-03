44e40e134536fdb3162557f7105edc1152d6fa6b
comtalyst <comtalyst@outlook.com>
2022-03-02 18:33:19 -0600

    Add ExpireClearMessage+Producer

---
+++ server/PersistentWorkers/Models/ExpireClearMessage.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace Chronoria_PersistentWorkers.Models
+{
+    public class ExpireClearMessage : IMessage
+    {
+        public DateTime TimeL { get; set; }
+        public DateTime TimeR { get; set; }
+
+        public ExpireClearMessage(DateTime TimeL, DateTime TimeR)
+        {
+            this.TimeL = TimeL;
+            this.TimeR = TimeR;
+        }
+        public string Jsonify()
+        {
+            var obj = new
+            {
+                _meta = new
+                {
+                    MessageClass = "ExpireClearMessage",
+                    Sender = "Chronoria-PersistentWorkers"
+                },
+                TimeL = TimeL,
+                TimeR = TimeR
+            };
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
---
+++ server/PersistentWorkers/Producers/ExpireClearProducer.cs
@@ -0,0 +1,9 @@
+﻿using Chronoria_PersistentWorkers.Models;
+
+namespace Chronoria_PersistentWorkers.Producers
+{
+    public class ExpireClearProducer : GeneralProducer<ExpireClearMessage>, IExpireClearProducer
+    {
+        public ExpireClearProducer(string connectionString) : base(connectionString, "ExpireClearTopic") { }
+    }
+}
