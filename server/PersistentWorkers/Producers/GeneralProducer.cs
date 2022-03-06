c76a6cca99e216a85e37292a273b0c314f8342b2
comtalyst <comtalyst@outlook.com>
2022-03-06 01:22:46 -0600

    Fix an issue where the producer is disposed right after sending one message

--- server/PersistentWorkers/Producers/GeneralProducer.cs
+++ server/PersistentWorkers/Producers/GeneralProducer.cs
@@ -1,9 +1,10 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Chronoria_PersistentWorkers.Models;
+using System;
 
 namespace Chronoria_PersistentWorkers.Producers
 {
-    public abstract class GeneralProducer<MessageType> : IGeneralProducer<MessageType> where MessageType : IMessage
+    public abstract class GeneralProducer<MessageType> : IAsyncDisposable, IGeneralProducer<MessageType> where MessageType : IMessage
     {
         protected readonly string connectionString;
         protected readonly string topicName;
@@ -17,26 +18,26 @@ namespace Chronoria_PersistentWorkers.Producers
             client = new ServiceBusClient(connectionString);
             sender = client.CreateSender(topicName);
         }
+
+        public async ValueTask DisposeAsync()
+        {
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
         public async Task Produce(MessageType message)
         {
             using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
             if (!messageBatch.TryAddMessage(new ServiceBusMessage(message.Jsonify()))){
                 throw new Exception($"The message is too large to fit in the batch.");
             }
-            try
-            {
-                await sender.SendMessagesAsync(messageBatch);
-            }
-            finally
-            {
-                await sender.DisposeAsync();
-                await client.DisposeAsync();
-            }
+            await sender.SendMessagesAsync(messageBatch);
         }
         public Task Produce(IEnumerable<MessageType> messages)
         {
             // TODO
             throw new NotImplementedException();
         }
+
     }
 }
