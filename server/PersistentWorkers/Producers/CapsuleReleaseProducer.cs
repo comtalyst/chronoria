a5d1e76b084ed8404b1e85e8bdf4df99188a2b01
comtalyst <comtalyst@outlook.com>
2022-03-05 20:03:14 -0600

    Add CapsuleReleaseProducer

---
+++ server/PersistentWorkers/Producers/CapsuleReleaseProducer.cs
@@ -0,0 +1,9 @@
+﻿using Chronoria_PersistentWorkers.Models;
+
+namespace Chronoria_PersistentWorkers.Producers
+{
+    public class CapsuleReleaseProducer : GeneralProducer<CapsuleReleaseMessage>, ICapsuleReleaseProducer
+    {
+        public CapsuleReleaseProducer(string connectionString) : base(connectionString, "CapsuleReleaseTopic") { }
+    }
+}
---
+++ server/PersistentWorkers/Producers/ICapsuleReleaseProducer.cs
@@ -0,0 +1,6 @@
+﻿using Chronoria_PersistentWorkers.Models;
+
+namespace Chronoria_PersistentWorkers.Producers
+{
+    public interface ICapsuleReleaseProducer : IGeneralProducer<CapsuleReleaseMessage> { }
+}
