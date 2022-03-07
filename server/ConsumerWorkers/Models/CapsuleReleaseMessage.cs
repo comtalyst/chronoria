ffb1ff6c09f3574052d14e4af6c2627d26ab8403
comtalyst <comtalyst@outlook.com>
2022-03-07 15:05:15 -0600

    Import messages

---
+++ server/ConsumerWorkers/Models/CapsuleReleaseMessage.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace Chronoria_ConsumerWorkers.Models
+{
+    public class CapsuleReleaseMessage : IMessage
+    {
+        public long TimeL { get; set; }
+        public long TimeR { get; set; }
+
+        public CapsuleReleaseMessage(long TimeL, long TimeR)
+        {
+            this.TimeL = TimeL;
+            this.TimeR = TimeR;
+        }
+        public string Jsonify()
+        {
+            var obj = new
+            {
+                _meta = new
+                {
+                    MessageClass = "CapsuleReleaseMessage",
+                    Sender = "Chronoria-PersistentWorkers"
+                },
+                TimeL = TimeL,
+                TimeR = TimeR
+            };
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
---
+++ server/ConsumerWorkers/Models/ExpireClearMessage.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace Chronoria_ConsumerWorkers.Models
+{
+    public class ExpireClearMessage : IMessage
+    {
+        public long TimeL { get; set; }
+        public long TimeR { get; set; }
+
+        public ExpireClearMessage(long TimeL, long TimeR)
+        {
+            this.TimeL = TimeL;
+            this.TimeR = TimeR;
+        }
+        public string Jsonify()
+        {
+            var obj = new
+            {
+                _meta = new
+                {
+                    MessageClass = "ExpireClearMessage",
+                    Sender = "Chronoria-PersistentWorkers"
+                },
+                TimeL = TimeL,
+                TimeR = TimeR
+            };
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
