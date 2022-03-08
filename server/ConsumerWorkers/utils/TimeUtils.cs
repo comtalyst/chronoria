1c1a4d9c2de81cd47051df47866e445d74e4f2b1
comtalyst <comtalyst@outlook.com>
2022-03-07 22:19:11 -0600

    Import TimeUtils

---
+++ server/ConsumerWorkers/utils/TimeUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace Chronoria_ConsumerWorkers.utils
+{
+    public class TimeUtils
+    {
+        public static long DateTimeToEpochMs(DateTime dt)
+        {
+            return new DateTimeOffset(dt).ToUnixTimeMilliseconds();
+        }
+        public static DateTime EpochMsToDateTime(long ms)
+        {
+            return DateTimeOffset.FromUnixTimeMilliseconds(ms).DateTime;
+        }
+        public static DateTime now()
+        {
+            return DateTime.Now.ToUniversalTime();
+        }
+    }
+}
