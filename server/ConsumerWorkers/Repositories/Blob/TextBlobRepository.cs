17e6a4c75482392403ab5529ef1b3615a387c52a
comtalyst <comtalyst@outlook.com>
2022-03-10 15:30:39 -0600

    Implement TextBlobRepository.Get()

--- server/ConsumerWorkers/Repositories/Blob/TextBlobRepository.cs
+++ server/ConsumerWorkers/Repositories/Blob/TextBlobRepository.cs
@@ -17,7 +17,11 @@ namespace Chronoria_ConsumerWorkers.Repositories
 
         public async Task<BlobText> Get(string blobFileName)
         {
-            throw new NotImplementedException();
+            BlobClient blobClient = GetClient(blobFileName);
+            var stream = await blobClient.OpenReadAsync();
+            StreamReader reader = new StreamReader(stream);
+            BlobText blobText = new BlobText(blobFileName, reader.ReadToEnd());
+            return blobText;
         }
     }
 }
