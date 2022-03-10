ad89f62bfaae8f6a691001a365a125ada086ce77
comtalyst <comtalyst@outlook.com>
2022-03-10 16:24:43 -0600

    Inject services

--- server/ConsumerWorkers/Program.cs
+++ server/ConsumerWorkers/Program.cs
@@ -35,25 +35,78 @@ builder.ConfigureServices((hostBuilderContext, services) =>
     var Configuration = hostBuilderContext.Configuration;
     // Services
     services.AddScoped<IExpireClearService, ExpireClearService>();
-
+    services.AddScoped<ICapsuleReleaseService, CapsuleReleaseService>();
 
     // Azure Service Bus Producers
-    /*services.AddScoped<IExpireClearProducer, ExpireClearProducer>(
-        sp => new ExpireClearProducer(Configuration["ServiceBus:Connections:Prime"]));
-    services.AddScoped<ICapsuleReleaseProducer, CapsuleReleaseProducer>(
-        sp => new CapsuleReleaseProducer(Configuration["ServiceBus:Connections:Prime"]));*/
+    services.AddScoped<ICapsuleDeliveryProducer, CapsuleDeliveryProducer>(
+        sp => new CapsuleDeliveryProducer(Configuration["ServiceBus:Connections:Prime"]));
 
     // Database Repositories
     services.AddScoped<ICapsuleRepository<PendingContext>, CapsuleRepository<PendingContext>>();
     services.AddScoped<ICapsuleRepository<ActiveContext>, CapsuleRepository<ActiveContext>>();
+    services.AddScoped<ICapsuleRepository<ArchivedContext>, CapsuleRepository<ArchivedContext>>();
+    services.AddScoped<ITextContentRepository<PendingContext>, TextContentRepository<PendingContext>>();
+    services.AddScoped<ITextContentRepository<ActiveContext>, TextContentRepository<ActiveContext>>();
+    services.AddScoped<ITextContentRepository<ArchivedContext>, TextContentRepository<ArchivedContext>>();
+    services.AddScoped<IFileContentRepository<PendingContext>, FileContentRepository<PendingContext>>();
+    services.AddScoped<IFileContentRepository<ActiveContext>, FileContentRepository<ActiveContext>>();
+    services.AddScoped<IFileContentRepository<ArchivedContext>, FileContentRepository<ArchivedContext>>();
 
     // Databases Contexts
     services.AddDbContext<PendingContext>(o => o.UseNpgsql(Configuration["Db:Connections:Pending"]));
     services.AddDbContext<ActiveContext>(o => o.UseNpgsql(Configuration["Db:Connections:Active"]));
+    services.AddDbContext<ArchivedContext>(o => o.UseNpgsql(Configuration["Db:Connections:Archived"]));
+
+    // Azure Blob Storage
+    services.AddSingleton<PendingBlobServiceClient>(new PendingBlobServiceClient(Configuration["Blob:Connections:Pending"]));
+    services.AddSingleton<ActiveBlobServiceClient>(new ActiveBlobServiceClient(Configuration["Blob:Connections:Active"]));
+    services.AddSingleton<ArchivedBlobServiceClient>(new ArchivedBlobServiceClient(Configuration["Blob:Connections:Archived"]));
+
+    services.AddScoped<IFileBlobRepository<ActiveBlobServiceClient>, FileBlobRepository<ActiveBlobServiceClient>>(
+        sp => new FileBlobRepository<ActiveBlobServiceClient>(
+            sp.GetRequiredService<ActiveBlobServiceClient>(),
+            Configuration["Blob:Containers:Active:File"]
+        )
+    );
+    services.AddScoped<IFileBlobRepository<PendingBlobServiceClient>, FileBlobRepository<PendingBlobServiceClient>>(
+        sp => new FileBlobRepository<PendingBlobServiceClient>(
+            sp.GetRequiredService<PendingBlobServiceClient>(),
+            Configuration["Blob:Containers:Pending:File"]
+        )
+    );
+    services.AddScoped<IFileBlobRepository<ArchivedBlobServiceClient>, FileBlobRepository<ArchivedBlobServiceClient>>(
+        sp => new FileBlobRepository<ArchivedBlobServiceClient>(
+            sp.GetRequiredService<ArchivedBlobServiceClient>(),
+            Configuration["Blob:Containers:Archived:File"]
+        )
+    );
+    services.AddScoped<ITextBlobRepository<ActiveBlobServiceClient>, TextBlobRepository<ActiveBlobServiceClient>>(
+        sp => new TextBlobRepository<ActiveBlobServiceClient>(
+            sp.GetRequiredService<ActiveBlobServiceClient>(),
+            Configuration["Blob:Containers:Active:Text"]
+        )
+    );
+    services.AddScoped<ITextBlobRepository<PendingBlobServiceClient>, TextBlobRepository<PendingBlobServiceClient>>(
+        sp => new TextBlobRepository<PendingBlobServiceClient>(
+            sp.GetRequiredService<PendingBlobServiceClient>(),
+            Configuration["Blob:Containers:Pending:Text"]
+        )
+    );
+    services.AddScoped<ITextBlobRepository<ArchivedBlobServiceClient>, TextBlobRepository<ArchivedBlobServiceClient>>(
+        sp => new TextBlobRepository<ArchivedBlobServiceClient>(
+            sp.GetRequiredService<ArchivedBlobServiceClient>(),
+            Configuration["Blob:Containers:Archived:Text"]
+        )
+    );
 
     // Azure Service Bus Consumers
     services.AddHostedService<ExpireClearConsumer>(
         sp => new ExpireClearConsumer(
+            Configuration["ServiceBus:Connections:Prime"], sp
+        )
+    );
+    services.AddHostedService<CapsuleReleaseConsumer>(
+        sp => new CapsuleReleaseConsumer(
             Configuration["ServiceBus:Connections:Prime"], sp
         )
     );
