86b7cc0a18a7228a02347517eafd3b8b9ec0c0d8
comtalyst <comtalyst@outlook.com>
2022-03-23 01:51:21 -0500

    WIP CoreEmailService

---
+++ server/ConsumerWorkers/Services/CoreEmailService.cs
@@ -0,0 +1,43 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Chronoria_ConsumerWorkers.Services
+{
+    public class CoreEmailService : ICoreEmailService
+    {
+        private readonly SendGridClient client;
+        private readonly string mainEmail;
+        private readonly string mainName;
+
+        public CoreEmailService(
+            string apiKey,
+            string mainEmail,
+            string mainName)
+        {
+            client = new SendGridClient(apiKey);
+            this.mainEmail = mainEmail;
+            this.mainName = mainName;
+        }
+
+        public async Task SendPlainText(string senderEmail, string senderName, string recipientEmail, string recipientName, string subject, string content)
+        {
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(senderEmail, senderName),
+                Subject = subject,
+                PlainTextContent = content
+            };
+            msg.AddTo(new EmailAddress(recipientEmail, recipientName));
+            var response = await client.SendEmailAsync(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Email fails to be delivered to " + recipientEmail + "; error: " + response.ToString());
+            }
+        }
+
+        public async Task SendPlainText(string recipientEmail, string recipientName, string subject, string content)
+        {
+            await SendPlainText(mainEmail, mainName, recipientEmail, recipientName, subject, content);
+        }
+    }
+}
---
+++ server/ConsumerWorkers/Services/ICoreEmailService.cs
@@ -0,0 +1,19 @@
+﻿namespace Chronoria_ConsumerWorkers.Services
+{
+    public interface ICoreEmailService
+    {
+        public Task SendPlainText(
+            string senderEmail,
+            string senderName,
+            string recipientEmail,
+            string recipientName,
+            string subject,
+            string content);
+        // use default email
+        public Task SendPlainText(
+            string recipientEmail,
+            string recipientName,
+            string subject,
+            string content);
+    }
+}
