e69819b3e8c8d108e8ef9170a8d55a6bc241610b
comtalyst <comtalyst@outlook.com>
2022-03-24 15:13:23 -0500

    Add a quick tester

---
+++ server/ConsumerWorkers.Tests/Services/CoreEmailServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using Xunit;
+using Chronoria_ConsumerWorkers.Services;
+
+namespace Chronoria_ConsumerWorkers.Tests.Services
+{
+    public class CoreEmailServiceTest : IDisposable
+    {
+        private readonly ICoreEmailService coreEmailService;
+
+        // this runs before every test
+        public CoreEmailServiceTest()
+        {
+            coreEmailService = new CoreEmailService("TODO", "timelette@timelette.app", "Timelette App");
+        }
+        // this runs after every test
+        public void Dispose() { }
+
+        [Fact]
+        public async void SendHtml()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string path = "../../../..";
+            string fullPath = Path.Combine(currentDirectory, path, "Email.html");
+            using (StreamReader r = new StreamReader(fullPath))
+            {
+                string html = r.ReadToEnd();
+                await coreEmailService.SendHtml("TODO:email", "TODO:name", "Hello HTML", html);
+            }
+        }
+    }
+}
