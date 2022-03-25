bd66078beebda0761d4e4404872d0ea1c8452dc2
comtalyst <comtalyst@outlook.com>
2022-03-25 00:37:03 -0500

    Implement EmailTemplateService

---
+++ server/ConsumerWorkers/Services/EmailTemplateService.cs
@@ -0,0 +1,19 @@
+﻿using Chronoria_ConsumerWorkers.Repositories;
+
+namespace Chronoria_ConsumerWorkers.Services
+{
+    public class EmailTemplateService : IEmailTemplateService
+    {
+        private readonly IEmailTemplateBlobRepository emailTemplateBlobRepository;
+        public EmailTemplateService(IEmailTemplateBlobRepository emailTemplateBlobRepository)
+        {
+            this.emailTemplateBlobRepository = emailTemplateBlobRepository;
+        }
+
+        public async Task<string> GetEmailTemplate(string templateName)
+        {
+            string templateFileName = templateName + "Email.html";
+            return (await emailTemplateBlobRepository.Get(templateFileName)).content;
+        }
+    }
+}
