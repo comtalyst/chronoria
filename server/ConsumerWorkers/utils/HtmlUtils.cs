f1beb9b184436697653cd237061db73047830d7d
comtalyst <comtalyst@outlook.com>
2022-03-25 00:36:35 -0500

    Add HTML extraction utils

---
+++ server/ConsumerWorkers/utils/HtmlUtils.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+
+namespace Chronoria_ConsumerWorkers.utils
+{
+    public class HtmlUtils
+    {
+        public static string? GetTitle(string htmlContent)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlContent);
+
+            var subjectAttr = htmlDoc.DocumentNode.ChildAttributes("title").FirstOrDefault();
+            if (subjectAttr == null)
+            {
+                return null;
+            }
+            string title = subjectAttr.Value;
+            return title;
+        }
+    }
+}
