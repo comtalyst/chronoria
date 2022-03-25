066b01996ac6f6c5d8f99ab5aba4024950370009
comtalyst <comtalyst@outlook.com>
2022-03-25 00:07:44 -0500

    Draft helper services

---
+++ server/ConsumerWorkers/Services/IEmailTemplateService.cs
@@ -0,0 +1,7 @@
+﻿namespace Chronoria_ConsumerWorkers.Services
+{
+    public interface IEmailTemplateService
+    {
+        public Task<string> GetEmailTemplate(string templateName);
+    }
+}
---
+++ server/ConsumerWorkers/Services/ILinkService.cs
@@ -0,0 +1,9 @@
+﻿namespace Chronoria_ConsumerWorkers.Services
+{
+    public interface ILinkService
+    {
+        public string GetConfirmationLink(string id);
+        public string GetDownloadLink(string id);
+        public string GetCancelationLink(string id);
+    }
+}
