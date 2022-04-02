3de004182044630a3533c32038dea262942577b7
comtalyst <comtalyst@outlook.com>
2022-04-02 17:24:00 -0500

    Use Flowbite for UI building assistance

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     fontFamily: {
@@ -17,5 +18,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
