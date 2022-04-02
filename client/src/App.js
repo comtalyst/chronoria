a9432e90cb8e5707692e8122602cecf04e1c6aac
comtalyst <comtalyst@outlook.com>
2022-04-02 00:20:47 -0500

    WIP (mostly done) landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo from './media/logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex min-h-screen bg-light_bg text-light_text text-3xl'>
+      <div className='flex-col px-20 py-12 w-1/2'>
+        <div className='mb-20'>
+          <span className='text-8xl font-black leading-snug'>
+            Time Travel with <br/>
+            <span className='text-light_hl'>
+              Timelette!
+            </span>
+          </span>
+        </div>
+        <div className='mb-20 max-w-[38rem]'>
+          <span className='text-4xl font-light leading-snug'>
+            A message, a hidden treasure map, a confession, a memory, or whatever it is.
+            Send it as <span className='text-light_hl font-bold'>a letter</span> to yourself or someone else from the future.
+          </span>
+        </div>
+        <div className='bg-light_hl w-fit px-5 py-2 text-light_hl_subtext font-bold text-2xl rounded
+                        hover:bg-light_hl_l hover:scale-105
+                        transition-all duration-200'>
+          Get Started
+        </div>
+      </div>
+      <button className='px-20 py-24 w-1/2'>
+        <img src={logo} alt='Timelette' className='hover:scale-95 transition-all duration-500'/>
+      </button>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light_bg: '#FFFFE0',
+        light_text: '#000000',
+        light_hl: '#FFAC06',
+        light_hl_l: '#FFDC46',
+        light_hl_subtext: '#FFFFFF'
+      }
+    },
   },
   plugins: [],
 }
