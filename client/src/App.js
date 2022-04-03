a28b2fee0d163630c7a8dcdf16ee9cfc46312236
comtalyst <comtalyst@outlook.com>
2022-04-03 18:12:27 -0500

    Change some font size + add error alert + WIP

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,12 @@ function App() {
   const [textRaw, setTextRaw] = useState('');
   const [fileRaw, setFileRaw] = useState(null);
   const [sendTimeRaw, setSendTimeRaw] = useState(null);
+  
+  const [submitting, setSubmitting] = useState(false);
+  const [currentWarning, setCurrentWarning] = useState('');
+  const [currentError, setCurrentError] = useState('');
 
-  const submit = async (e) => {
+  const trueSubmit = async (e) => {
     e.preventDefault();
     const senderEmail = senderEmailRaw.trim();
     const senderName = senderNameRaw.trim();
@@ -30,11 +34,11 @@ function App() {
     const text = textRaw.trim();
     const sendTime = Date.parse(sendTimeRaw);
     if(sendTime < Date.now()){
-      alert('Sorry, but we cannot send a letter to the past at this time (but we wish we could ;-; )');
+      setCurrentWarning('Sorry, but we cannot send a letter to the past at this time (but we wish we could ;-; )');
       return false;
     }
     else if(sendTime < Date.now() + 172800000){
-      alert('Destination time needs to be at least 2 days from now');
+      setCurrentWarning('Destination time needs to be at least 2 days from now');
       return false;
     }
     if(fileRaw == null){
@@ -42,17 +46,11 @@ function App() {
       const reqBody = {
         senderEmail, senderName, recipientEmail, recipientName, sendTime, text
       }
-      try{
-        const res = await axios.post('https://localhost:7056/api/submit/text', reqBody);
-        console.log(res);
-      } catch (ex){
-        console.error(ex.message);
-      }
+      await axios.post('https://localhost:7056/api/submit/text', reqBody);
     }
     else{
       if(fileRaw.size > 200000000){
-        alert('File size must not exceed 200 MB');
-        return false;
+        throw new Error('File size must not exceed 200 MB');
       }
       const reqBody = {
         senderEmail, senderName, recipientEmail, recipientName, sendTime, text
@@ -65,18 +63,32 @@ function App() {
       }
       formData.append('FormFile', fileRaw);
       formData.append('FileName', fileRaw.name);
-      try{
-        const res = await axios.post('https://localhost:7056/api/submit/file', formData);
-        console.log(res);
-      } catch (ex){
-        console.error(ex.message);
-      }
+      await axios.post('https://localhost:7056/api/submit/file', formData);
     }
+    return true;
+  }
+  const submit = async (e) => {
+    e.preventDefault();
+    setCurrentError('');
+    setCurrentWarning('');
+    setSubmitting(true);
+    try{
+      const res = await trueSubmit(e);
+      if(!res){
+        setSubmitting(false);
+        return false;
+      }
+    } catch (e){
+      setCurrentError(e.message);
+      setSubmitting(false);
+      return false;
+    }
+    // TODO: do something
     return true;
   }
   
   return (
-    <div className='flex flex-col min-h-screen bg-light_bg text-light_text text-xl font-light'>
+    <div className='flex flex-col min-h-screen bg-light_bg text-light_text text-lg'>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap" rel="stylesheet"></link>
@@ -113,9 +125,21 @@ function App() {
       </div>
 
       <div className='flex flex-col min-h-screen px-6 md:px-24 pt-12'>
-        <div className='flex flex-col mb-10'>
+        <div className='flex flex-col mb-8'>
+          { (currentWarning === '')? (<div/>) : (
+              <div class="p-4 mx-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded" role="alert">
+                <span class="font-semibold">Warning!</span> {currentWarning}
+              </div>
+            )
+          }
+          { (currentError === '')? (<div/>) : (
+              <div class="p-4 mx-4 mb-4 text-sm text-red-700 bg-red-100 rounded" role="alert">
+                <span class="font-semibold">Error!</span> {currentError}
+              </div>
+            )
+          }
           <form onSubmit={submit}>
-            <div className='flex flex-wrap gap-y-4 justify-between mb-10'>
+            <div className='flex flex-wrap gap-y-4 justify-between mb-8'>
               <div className='flex flex-col min-w-[50%] grow space-y-3 px-4'>
                 <label htmlFor='senderEmail'>
                   Sender's Email (yes, yours!)
@@ -135,11 +159,11 @@ function App() {
             <div className='flex mb-3 px-4 items-center'>
               <input id='selfSend' aria-describedby='selfSend' type='checkbox' onChange={() => setSelfSend(!selfSend)}
                 className='w-4 h-4 rounded border border-gray-300 focus:ring-3 focus:ring-light_hl '/>
-              <label htmlFor='selfSend' className='text-base pl-3'>This letter is for myself</label>
+              <label htmlFor='selfSend' className='text-sm pl-3'>This letter is for myself</label>
             </div>
 
             {(!selfSend)? (
-            <div className='flex flex-wrap gap-y-4 justify-between mb-10'>
+            <div className='flex flex-wrap gap-y-4 justify-between mb-8'>
               <div className='flex flex-col min-w-[50%] grow space-y-3 px-4'>
                 <label htmlFor='recipientEmail'>
                   Recipient's Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -157,7 +181,7 @@ function App() {
             </div>
             ):(<div/>)}
 
-            <div className='flex flex-col mb-10 space-y-3 px-4'>
+            <div className='flex flex-col mb-8 space-y-3 px-4'>
               <label htmlFor='text'>
                 Inside the Envelope
               </label>
@@ -165,10 +189,10 @@ function App() {
                 className='w-full border border-gray-300 text-gray-900 rounded focus:ring-light_hl focus:border-light_hl'/>
               <input className='text-sm text-gray-900 bg-white rounded border border-gray-300 cursor-pointer 
                 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-light_hl' id='fileUpload' type='file' onChange={(e) => setFileRaw(e.target.files[0])}/>
-              <label htmlFor='fileUpload' className='text-base'>File upload is optional. The maximum allowed file size is 200 MB. Recommended extensions: .pdf, .txt, .jpg, .mp4, .zip</label>
+              <label htmlFor='fileUpload' className='text-sm text-gray-500'>File upload is optional. The maximum allowed file size is 200 MB. Recommended extensions: .pdf, .txt, .jpg, .mp4, .zip</label>
             </div>
 
-            <div className='flex flex-col mb-10 space-y-3'>
+            <div className='flex flex-col mb-8 space-y-3'>
               <div className='flex flex-col max-w-[18rem] space-y-3 px-4'>
                 <label htmlFor='sendTimeRaw'>
                   Destination Time
@@ -176,19 +200,29 @@ function App() {
                 <input type='datetime-local' className='text-gray-900 bg-white rounded border border-gray-300 focus:ring-light_hl focus:border-light_hl' 
                   placeholder='Select date' required onChange={(e) => setSendTimeRaw(e.target.value)}/>
               </div>
-              <label htmlFor='sendTimeRaw' className='px-4 -mt-10 text-base'>Date and time is based on your local machine time zone. It needs to be at least 2 days from now.</label>
+              <label htmlFor='sendTimeRaw' className='px-4 -mt-10 text-sm text-gray-500'>Date and time is based on your local machine time zone. It needs to be at least 2 days from now.</label>
             </div>
 
             
 
-            <div className='flex flex-col mb-10 space-y-3 px-4'>
-              <span className='text-base'>By clicking submit, you agree to the <a className='text-light_hl underline' href='https://google.com'>terms and conditions</a>.</span>
-              <button className='bg-light_hl w-fit px-5 py-2 text-light_hl_subtext font-bold text-2xl rounded
+            <div className='flex flex-col mb-8 space-y-3 px-4'>
+              <span className='text-sm text-gray-500'>By clicking submit, you agree to the <a className='text-light_hl underline' href='https://google.com'>terms and conditions</a>.</span>
+              {
+                (!submitting)? (
+                  <button className='bg-light_hl w-fit px-5 py-2 text-light_hl_subtext font-bold text-2xl rounded
                                  hover:bg-light_hl_l hover:scale-105
                                  transition-all duration-200'
-                      type='submit'>
-                Submit
-              </button>
+                          type='submit'>
+                    Submit
+                  </button>
+                ) : (
+                  <button className='bg-gray-400 w-fit px-5 py-2 text-light_hl_subtext font-bold text-2xl rounded
+                                 transition-all duration-200'
+                          type='submit' disabled>
+                    Processing...
+                  </button>
+                )
+              }
             </div>
           </form>
         </div>
