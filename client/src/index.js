617d02d4f5dc4bc8b56d6f7d00abb278b5e9f05c
comtalyst <comtalyst@outlook.com>
2022-04-04 22:12:13 -0500

    Implement React Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,11 @@ ReactDOM.render(
     <link rel='preconnect' href='https://fonts.googleapis.com'/>
     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true'/>
     <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap' rel='stylesheet'></link>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
