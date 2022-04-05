dcdcf0b5ae13da54185fbb571af1cb8227298fc1
comtalyst <comtalyst@outlook.com>
2022-04-05 02:15:06 -0500

    Add terms of service

--- client/src/App.js
+++ client/src/App.js
@@ -290,7 +290,7 @@ function App() {
                 </div>
                 <div className='p-6 space-y-6'>
                   <p className='text-base leading-relaxed text-light_text_l'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    {dialogues.terms}
                   </p>
                 </div>
               </div>
