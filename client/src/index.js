d6ed147ee447147e95906537867df9f76e018aa5
comtalyst <comtalyst@outlook.com>
2022-04-05 01:17:39 -0500

    Add confirmation page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Confirm from './Confirm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -13,6 +14,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='confirm/:id' element={<Confirm/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
